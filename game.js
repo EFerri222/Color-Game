b1f3688105cf099feeae05b95bc948c856291ccb
EFerri222 <eferri222@gmail.com>
2020-03-02 17:54:22 -0500

    set each square to be a different static color, changed body background to a shade of grey, added h1 element

---
+++ game.js
@@ -0,0 +1,12 @@
+var colors = [
+    "rgb(255, 0, 0)",
+    "rgb(255, 255, 0)",
+    "rgb(0, 255, 0)",
+    "rgb(0, 255, 255)",
+    "rgb(0, 0, 255)",
+    "rgb(255, 0, 255)"
+]
+
+for (var i = 0; i < colors.length; i++) {
+    document.querySelector("#square" + i).setAttribute("style", "background: " + colors[i]);
+}
