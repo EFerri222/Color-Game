32d42c403a7681dbc80f445df995efc88ed11300
EFerri222 <eferri222@gmail.com>
2020-03-03 00:09:46 -0500

    Add new colors/play again functionality

--- game.js
+++ game.js
@@ -3,6 +3,8 @@ var squares = document.querySelectorAll(".square");
 var correctSquare = squares[Math.floor(Math.random() * squares.length)];
 var rgbDisplay = document.querySelector("#rgb-display");
 var messageDisplay = document.querySelector("#message");
+var h1 = document.querySelector("h1");
+var newColorsBtn = document.querySelector("#new-colors-btn");
 var gameOver = false;
 
 // Create a random RGB value string
@@ -15,28 +17,52 @@ function randomColor() {
 }
 
 // Give each square a random background color
-for (var i = 0; i < squares.length; i++) {
-    squares[i].style.backgroundColor = randomColor();
+function newColors() {
+    for (var i = 0; i < squares.length; i++) {
+        squares[i].style.backgroundColor = randomColor();
+    }
 }
 
-// Display rgb value of the correct square in HTML
-rgbDisplay.textContent = correctSquare.style.backgroundColor.toUpperCase();
+newColors();
 
+// Display rgb value of the correct square in HTML
+rgbDisplay.textContent = correctSquare.style.backgroundColor.toUpperCase()
+
+// For each square...
 for(var i = 0; i < squares.length ; i++){
     squares[i].addEventListener("click", function() {
         if(!gameOver) {
-            // If incorrect square is clicked, it becomes the same color as the body
-            if(this != correctSquare) {
+            // ...if incorrect square is clicked, it becomes the same color as the body
+            if(this !== correctSquare) {
                 this.style.backgroundColor = "#232323"
                 messageDisplay.textContent = "Try Again!";
-            // Otherwise make all the squares the correct color
+            // Otherwise make all the squares and h1 background the correct color
             } else {
                 for(var i = 0; i < squares.length ; i++){
                     squares[i].style.backgroundColor = correctSquare.style.backgroundColor;
                 }
                 messageDisplay.textContent = "Correct!";
+                h1.style.backgroundColor = this.style.backgroundColor;
+                newColorsBtn.textContent = "Play Again?";
+                // Game is over
                 gameOver = true;
             }
         }
     })
 }
+
+// Pick new correct square
+function newCorrectSquare() {
+    correctSquare = squares[Math.floor(Math.random() * squares.length)];
+        // Display rgb value of the correct square in HTML
+        rgbDisplay.textContent = correctSquare.style.backgroundColor.toUpperCase()
+}
+
+newColorsBtn.addEventListener("click", function() {
+    gameOver = false;
+    newColors();
+    newCorrectSquare();
+    h1.style.backgroundColor = "#232323";
+    newColorsBtn.textContent = "New Colors";
+    messageDisplay.textContent = "";
+})
