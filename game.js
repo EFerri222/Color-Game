06d8ca1c4f91b6e8b997c4cb11949d791373899a
EFerri222 <eferri222@gmail.com>
2020-03-02 21:18:55 -0500

    Add instructions div, refactor JS

--- game.js
+++ game.js
@@ -1,3 +1,8 @@
+// Global variables
+var squares = document.querySelectorAll(".square");
+var correctSquare = squares[Math.floor(Math.random() * 6)];
+var rgbDisplay = document.querySelector("#rgb-display");
+var messageDisplay = document.querySelector("#message");
 var gameOver = false;
 
 // Create a random RGB value string
@@ -14,23 +19,13 @@ function randomColor() {
     return "rgb(" + red + ", " + green + ", " + blue + ")";
 }
 
-// Select all squares by class name
-var squares = document.querySelectorAll(".square");
-
 // Give each square a random background color
 for (var i = 0; i < squares.length; i++) {
     squares[i].style.backgroundColor = randomColor();
 }
 
-// Choose random number between 0 and 5
-var answerIndex = Math.floor(Math.random() * 6);
-
-// Choose correct square
-var correctSquare = squares[answerIndex];
-
 // Display rgb value of the correct square in HTML
-var rgbDisplay = document.querySelector("#rgb-display");
-rgbDisplay.textContent = squares[answerIndex].style.backgroundColor.toUpperCase();
+rgbDisplay.textContent = correctSquare.style.backgroundColor.toUpperCase();
 
 for(var i = 0; i < squares.length ; i++){
     squares[i].addEventListener("click", function() {
@@ -38,11 +33,13 @@ for(var i = 0; i < squares.length ; i++){
             // If incorrect square is clicked, it becomes the same color as the body
             if(this != correctSquare) {
                 this.style.backgroundColor = "#232323"
+                messageDisplay.textContent = "Try Again!";
             // Otherwise make all the squares the correct color
             } else {
                 for(var i = 0; i < squares.length ; i++){
                     squares[i].style.backgroundColor = correctSquare.style.backgroundColor;
                 }
+                messageDisplay.textContent = "Correct!";
                 gameOver = true;
             }
         }
