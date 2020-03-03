a99bcd699c03fd036f6ff6362eba2179a5cac025
EFerri222 <eferri222@gmail.com>
2020-03-03 01:33:34 -0500

    Make 3 difficulty modes

--- game.js
+++ game.js
@@ -6,6 +6,7 @@ var messageDisplay = document.querySelector("#message");
 var h1 = document.querySelector("h1");
 var resetBtn = document.querySelector("#reset-btn");
 var easyBtn = document.querySelector("#easy-btn");
+var medBtn = document.querySelector("#med-btn");
 var hardBtn = document.querySelector("#hard-btn");
 var gameOver = false;
 
@@ -79,14 +80,48 @@ resetBtn.addEventListener("click", function() {
 });
 
 easyBtn.addEventListener("click", function(){
-    document.querySelector("#container").innerHTML = "<div class=\"square\"></div><div class=\"square\"></div><div class=\"square\"></div>";
+    easyBtn.classList.add("selected");
+    medBtn.classList.remove("selected");
+    hardBtn.classList.remove("selected");
+    document.querySelector("#container").innerHTML =
+    "<div class=\"square\"></div>" +
+    "<div class=\"square\"></div>" +
+    "<div class=\"square\"></div>";
+    squares = document.querySelectorAll(".square");
+    reset();
+    addClickEvents();
+});
+
+medBtn.addEventListener("click", function(){
+    easyBtn.classList.remove("selected");
+    medBtn.classList.add("selected");
+    hardBtn.classList.remove("selected");
+    document.querySelector("#container").innerHTML =
+    "<div class=\"square\"></div>" +
+    "<div class=\"square\"></div>" +
+    "<div class=\"square\"></div>" +
+    "<div class=\"square\"></div>" +
+    "<div class=\"square\"></div>" +
+    "<div class=\"square\"></div>";
     squares = document.querySelectorAll(".square");
     reset();
     addClickEvents();
 });
 
 hardBtn.addEventListener("click", function(){
-    document.querySelector("#container").innerHTML = "<div class=\"square\"></div><div class=\"square\"></div><div class=\"square\"></div><div class=\"square\"></div><div class=\"square\"></div><div class=\"square\"></div>";
+    easyBtn.classList.remove("selected");
+    medBtn.classList.remove("selected");
+    hardBtn.classList.add("selected");
+    document.querySelector("#container").innerHTML =
+    "<div class=\"square\"></div>" +
+    "<div class=\"square\"></div>" +
+    "<div class=\"square\"></div>" +
+    "<div class=\"square\"></div>" +
+    "<div class=\"square\"></div>" +
+    "<div class=\"square\"></div>" +
+    "<div class=\"square\"></div>" +
+    "<div class=\"square\"></div>" +
+    "<div class=\"square\"></div>";
     squares = document.querySelectorAll(".square");
     reset();
     addClickEvents();
