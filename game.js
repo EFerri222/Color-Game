f139c1ced0a7d31dc99a7e0d40f753f05ba6b8fb
EFerri222 <eferri222@gmail.com>
2020-03-02 19:20:21 -0500

    Assign each square a random background color

--- game.js
+++ game.js
@@ -1,14 +1,21 @@
-var colors = [
-    "rgb(255, 0, 0)",
-    "rgb(255, 255, 0)",
-    "rgb(0, 255, 0)",
-    "rgb(0, 255, 255)",
-    "rgb(0, 0, 255)",
-    "rgb(255, 0, 255)"
-]
+// Create a random RGB value string
+function randomColor() {
+    function getRandomIntInclusive(min, max) {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+    var red = getRandomIntInclusive(0,255);
+    var green = getRandomIntInclusive(0,255);
+    var blue = getRandomIntInclusive(0,255);
 
+    return "rgb(" + red + ", " + green + ", " + blue + ")";
+}
+
+// Select all squares by class name
 var squares = document.querySelectorAll(".square");
 
+// Give each square a random background color
 for (var i = 0; i < squares.length; i++) {
-    squares[i].style.backgroundColor = colors[i];
+    squares[i].style.backgroundColor = randomColor();
 }
